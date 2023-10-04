f123f709f2ea74539731d2e7e633cb7d21afc0aa
Vinicius <vinicius@iconax.com.br>
2023-10-04 10:18:42 -0300

    criando projeto spring

---
+++ src/main/java/com/dio/restapi/RestApiUsingSwaggerApplication.java
@@ -0,0 +1,13 @@
+package com.dio.restapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiUsingSwaggerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestApiUsingSwaggerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dio/restapi/RestApiUsingSwaggerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dio.restapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiUsingSwaggerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
